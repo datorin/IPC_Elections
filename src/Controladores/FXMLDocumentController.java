969f329cfbe6b0df2e597b3f312407347d157607
EVIL GOFRE <danieltorresingles@gmail.com>
2017-05-24 14:37:05 +0200

    PieChart acabada.

--- src/Controladores/FXMLDocumentController.java
+++ src/Controladores/FXMLDocumentController.java
@@ -5,8 +5,7 @@
  */
 package Controladores;
 
-import electionresults.model.ElectionResults;
-import electionresults.model.Province;
+import electionresults.model.Party;
 import electionresults.model.ProvinceInfo;
 import electionresults.persistence.io.DataAccessLayer;
 import java.net.URL;
@@ -18,6 +17,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
@@ -25,8 +25,6 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Separator;
-import javafx.scene.input.MouseEvent;
-import javax.swing.plaf.synth.Region;
 
 /**
  * FXML Controller class
@@ -49,75 +47,99 @@ public class FXMLDocumentController implements Initializable {
     private BarChart<?, ?> barChartVotos;
 
     private List<Integer> anyos;
-    
-    private String provincia;
-    
-    private Region region;
-    
-    private Province TODAS;
-    
-    private ElectionResults er;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         anyosVotos();
-        provinciaVotos((Integer)comboAnyoVotos.getSelectionModel().getSelectedItem());
-        regionVotos((String)comboProvVotos.getSelectionModel().getSelectedItem());
+        provinciaVotos((Integer) comboAnyoVotos.getSelectionModel().getSelectedItem());
+        regionVotos((String) comboProvVotos.getSelectionModel().getSelectedItem());
         comboAnyoVotos.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                provinciaVotos((Integer)comboAnyoVotos.getItems().get(newValue.intValue()));
+                provinciaVotos((Integer) comboAnyoVotos.getItems().get(newValue.intValue()));
             }
         });
         comboProvVotos.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (newValue.intValue() != -1){
-                    regionVotos((String)comboProvVotos.getItems().get(newValue.intValue()));
+                if (newValue.intValue() != -1) {
+                    regionVotos((String) comboProvVotos.getItems().get(newValue.intValue()));
                 }
             }
         });
-    }    
-    
+    }
+
     private void anyosVotos() {
-        comboAnyoVotos.getItems().clear();
         anyos = DataAccessLayer.getElectionYears();
         ObservableList<Integer> ob = FXCollections.observableList(anyos);
         comboAnyoVotos.setItems(ob);
         comboAnyoVotos.getSelectionModel().select(0);
     }
-    
+
     private void provinciaVotos(Integer anyo) {
         comboProvVotos.getItems().clear();
         ArrayList<String> arrayProvincia = new ArrayList<>();
-        Map<String,ProvinceInfo> mapProv;
+        Map<String, ProvinceInfo> mapProv;
         mapProv = DataAccessLayer.getElectionResults(anyo).getProvinces();
         for (String s : mapProv.keySet()) {
             arrayProvincia.add(mapProv.get(s).getProvince());
         }
         ObservableList<Object> ob = FXCollections.observableArrayList(arrayProvincia);
-        ob.add(0,new Separator());
-        ob.add(0,"COM. VALENCIANA");
+        ob.add(0, new Separator());
+        ob.add(0, "COM. VALENCIANA");
         comboProvVotos.setItems(ob);
         comboProvVotos.getSelectionModel().select(0);
     }
-    
+
     private void regionVotos(String nombreProvincias) {
         List<String> arrayRegion = new ArrayList<>();
         if (nombreProvincias.equals("COM. VALENCIANA")) {
             comboRegVotos.getItems().clear();
             comboRegVotos.setDisable(true);
-        }else {
+        } else {
             comboRegVotos.setDisable(false);
             arrayRegion = DataAccessLayer.getElectionResults(comboAnyoVotos.getSelectionModel().getSelectedItem()).getProvinces()
-                                         .get(nombreProvincias).getRegions();
+                    .get(nombreProvincias).getRegions();
         }
         ObservableList<Object> ob = FXCollections.observableArrayList(arrayRegion);
-        ob.add(0,new Separator());
-        ob.add(0,"REGIONES");
+        ob.add(0, new Separator());
+        ob.add(0, "REGIONES");
         comboRegVotos.setItems(ob);
         comboRegVotos.getSelectionModel().select(0);
     }
+
+    @FXML
+    private void onCalcular(ActionEvent event) {
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        for (Party p : Party.values()) {
+            try {
+                double dp = DataAccessLayer.getElectionResults(comboAnyoVotos.getSelectionModel().getSelectedItem()).getGlobalResults().getPartyResults(p.getName()).getSeats();
+                pieChartData.add(new PieChart.Data(p.getName(), dp));
+                System.out.println("1");
+            } catch (NullPointerException e) {
+            }
+        }
+        pieChartVotos.setData(pieChartData);
+    }
+
+    /*private void calculoDeDatos() {
+        PieChart pc;
+        if (comboProvVotos.getSelectionModel().getSelectedItem().equals("COM. VALENCIANA")) {
+            for (Party p : Party.values()) {
+                PartyResults dp = DataAccessLayer.getElectionResults(comboAnyoVotos.getSelectionModel().getSelectedItem()).getGlobalResults().getPartyResults(p.getName());
+                pc.add(new PieChart.Data(p.getName(),dp.getSeats()));
+            }
+        } else {
+            if (comboProvVotos.getSelectionModel().getSelectedItem().equals("REGIONES")) {
+                DataAccessLayer.getElectionResults(comboAnyoVotos.getSelectionModel().getSelectedItem()).
+                        getProvinceResults((String)comboProvVotos.getSelectionModel().getSelectedItem());
+            } else {
+                DataAccessLayer.getElectionResults(comboAnyoVotos.getSelectionModel().getSelectedItem()).
+                        getRegionResults((String)comboRegVotos.getSelectionModel().getSelectedItem());
+            }
+        }
+    }*/
 }
